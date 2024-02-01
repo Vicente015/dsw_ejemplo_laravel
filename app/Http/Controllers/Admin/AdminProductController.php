503471dd59d16eb186b2822d4b28e0d8b2a24317
Vicente <33096355+Vicente015@users.noreply.github.com>
2024-02-01 12:39:50 +0000

    Guarda y procesa imágenes

--- app/Http/Controllers/Admin/AdminProductController.php
+++ app/Http/Controllers/Admin/AdminProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Models\Product;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class AdminProductController extends Controller
 {
@@ -18,21 +19,34 @@ class AdminProductController extends Controller
 
     public function post(Request $request)
     {
-        //dd($request->all());
-
         $request->validate([
             'name' => 'required|max:255',
             'description' => 'required',
-            //'image' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg|max:2048',
             'price' => 'required|numeric|min:0',
         ]);
+
+        if ($request->hasFile('image')) {
+            // Obtener siguiente ID en la db
+            $productId = Product::max('id') + 1;
+            $imageName = $productId . '_' . $request->file('image')->getClientOriginalExtension();
+
+            // Almacenar la imagen
+            Storage::disk('public')->put(
+                $imageName,
+                file_get_contents($request->file('image')->getRealPath())
+            );
+
+            // Ruta de la imagen
+            $imagePath = 'storage/' . $imageName;
+        }
 
         // Crear objeto Product
         $product = new Product;
 
         $product->name = $request->input('name');
         $product->description = $request->input('description');
-        $product->image = ""; //$request->input('image');
+        $product->image = isset($imagePath) ? $imagePath : null;
         $product->price = $request->input('price');
 
         // Guardar el producto
