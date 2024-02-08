b2c4adbe0b3c8d333a8643d3b9680148bfb80963
Vicente <33096355+Vicente015@users.noreply.github.com>
2024-02-08 19:43:32 +0000

    Implementa editar y borrar producto

--- app/Http/Controllers/Admin/AdminProductController.php
+++ app/Http/Controllers/Admin/AdminProductController.php
@@ -55,4 +55,61 @@ class AdminProductController extends Controller
 
         return redirect()->route('admin.product.index')->with('success', 'Datos válidos y procesados correctamente');
     }
+
+    public function edit(string $id)
+    {
+        $viewData = [];
+        $viewData["title"] = "Admin Page - Products - Edit";
+        $viewData["product"] = Product::find($id);
+        $viewData["products"] = Product::all();
+        return view('admin.product.edit')->with('viewData', $viewData);
+    }
+
+    public function update(Request $request)
+    {
+        $request->validate([
+            'id' => 'required',
+            'name' => 'required|max:255',
+            'description' => 'required',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+            'price' => 'required|numeric|min:0',
+        ]);
+
+        $product = Product::find($request->input('id'));
+
+        if ($request->hasFile('image')) {
+            // Borra la imagen existente si hay
+            if ($product->image) {
+                Storage::disk('public')->delete($product->image);
+            }
+
+            $imageName = $product->id . '_' . $request->file('image')->getClientOriginalName() . $request->file('image')->getExtension();
+
+            // Almacenar la imagen
+            Storage::disk('public')->put(
+                $imageName,
+                file_get_contents($request->file('image')->getRealPath())
+            );
+
+            // Ruta de la imagen
+            $imagePath = 'storage/' . $imageName;
+        }
+
+        $product->name = $request->input('name');
+        $product->description = $request->input('description');
+        $product->image = isset($imagePath) ? $imagePath : $product->image;
+        $product->price = $request->input('price');
+
+        // Guardar el producto
+        $product->save();
+
+        return redirect()->route('admin.product.edit', $product->id)->with('success', 'Producto editado correctamente');
+    }
+
+    public function destroy(Request $request)
+    {
+        $productId = $request->input('id');
+        Product::destroy($productId);
+        return redirect()->route('admin.product.index')->with('success', 'Producto eliminado correctamente');
+    }
 }
